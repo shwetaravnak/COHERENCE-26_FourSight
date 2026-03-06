5d18c6770e0f998fedcbad0b4e697e47c3499ceb
Piyagaikwad <bewkoof0521@gmail.com>
2026-03-06 19:39:50 +0530

    Database

---
+++ backend/database.py
@@ -0,0 +1,34 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+
+BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+DB_PATH  = os.path.join(BASE_DIR, "../data/clinmatch.db")
+DATABASE_URL = f"sqlite:///{DB_PATH}"
+
+engine = create_engine(
+    DATABASE_URL,
+    connect_args={"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+    
+def init_db():
+    from models import Patient, Trial, MatchResult, Inquiry, User
+    Base.metadata.create_all(bind=engine)
+    print("✅ Database tables created")
